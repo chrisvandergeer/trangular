8ced7340498509f8791697ae32c5d995d2f1ab0b
Chris van der Geer <chris@MacBook-Air-van-Chris.local>
2018-05-29 21:43:06 +0200

    transaktie.ts compleet gemaakt

--- src/app/transaktie/transaktie.ts
+++ src/app/transaktie/transaktie.ts
@@ -1,7 +1,17 @@
 export class Transaktie {
 
-  datum: string;
-  omschrijving: string;
+  jaar: number;
+  maand: number;
+  dag: number;
+  debetCredit: string;
   bedrag: number;
+  tegenrekeningNummer: string;
+  tegenrekeningNaam: string;
+  soortTransaktie: string;
+  omschrijving1: string;
+  omschrijving2: string;
+  omschrijving3: string;
+  omschrijving4: string;
+  tags: string[];
 
 }
